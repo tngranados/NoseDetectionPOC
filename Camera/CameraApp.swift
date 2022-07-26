5083f2e4e57963948df47445cf21afa3333ad1d1
Toni Granados <toni@alkimii.com>
2022-07-26 11:32:06 +0200

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Antonio Granados Moscoso on 26/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CameraView()
+        }
+    }
+}
---
+++ Camera/CameraView.swift
@@ -0,0 +1,21 @@
+//
+//  CameraView.swift
+//  Camera
+//
+//  Created by Antonio Granados Moscoso on 26/7/22.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
